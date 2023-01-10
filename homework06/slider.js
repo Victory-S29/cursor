646f45eeae1ccab819ac69bb6fbd5dd51649898b
Вікторія Саніна <viktoriia.sanina.2917@gmail.com>
2023-01-10 17:27:45 +0200

    added-third-and-fourth-areas

---
+++ homework06/slider.js
@@ -0,0 +1,10 @@
+$(".slider-custom").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    prevArrow: $(".prev"),
+    nextArrow: $(".next"),
+ 
+    adaptiveHeight: true,
+    });
